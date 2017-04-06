a5e3b0539c37c66480004d46bf56565ef2d7bf22
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-04-06 14:18:06 +0000

    Added a home page by creating a new controller, view, and route

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+   
+    # The corresponding view file will need to be called home.html.erb, and will belong in the "pages" folder
+    def home
+       
+    end
+    
+end
