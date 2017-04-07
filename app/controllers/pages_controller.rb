23066ef6101d9166aa36219af2da91eb38b2aa98
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-04-07 10:05:56 +0000

    Added an about page, styled the navbar, and added CSS to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
    
-    # The corresponding view file will need to be called home.html.erb, and will belong in the "pages" folder
+    # The corresponding view file will need to be called home.html.erb, and will belong in the "pages" folder.
     def home
        
+    end
+    
+    # The corresponding view file will need to be called about.html.erb, and will belong in the "pages" folder.
+    def about
+        
     end
     
 end
