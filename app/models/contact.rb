016b8003c11c2459c6818e4936f4ee1976c36bbd
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-04-10 18:31:37 +0000

    Added form validations for contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
+    
+    validates :name, length: { minimum: 2, too_short: "Please enter a name longer than 1 letter" }
     
 end
