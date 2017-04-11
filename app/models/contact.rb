70502d4e9846faad5369501b3073eb4960e71eb4
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-04-11 08:29:06 +0000

    Made changes to contact_form routes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,6 @@ class Contact < ActiveRecord::Base
     validates :email, presence: true
     validates :comments, presence: true
     
-    validates :name, length: { minimum: 2, too_short: "Please enter a name longer than 1 letter" }
+    validates :name, length: { minimum: 2, too_short: "must be longer than 1 letter" }
     
 end
