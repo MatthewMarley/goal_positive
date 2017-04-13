fdf09e4c5bd191cacbfea7191347db9c851b1f5d
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-04-13 19:18:39 +0000

    Amended home page to include signup button

--- db/migrate/20170413095128_add_plan_to_user.rb
+++ db/migrate/20170413095128_add_plan_to_user.rb
@@ -1,5 +1,6 @@
 class AddPlanToUser < ActiveRecord::Migration[5.0]
   def change
+    # Add a column to the users table, called plan_id, and have it be in integer format
     add_column :users, :plan_id, :integer
   end
 end
