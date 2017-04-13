a822ca930c16715424a147dbc587dd352ba45907
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-04-13 19:40:14 +0000

    Added plan :id params to sign_up page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
    
     # The corresponding view file will need to be called home.html.erb, and will belong in the "pages" folder.
     def home
-       
+        @basic_plan = Plan.find(1)
     end
     
     # The corresponding view file will need to be called about.html.erb, and will belong in the "pages" folder.
