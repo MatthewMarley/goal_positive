81ffd83f63012323ee078f861235cca442cde567
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-04-18 09:51:24 +0000

    Added goal homepage where you can view all current goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -5,10 +5,32 @@ class GoalsController < ApplicationController
     end
     
     
+    def create
+        @user = User.find(params[:user_id])
+        @goal = @user.goals.build(goal_params)
+        
+        if @goal.save
+            flash[:success] = "Goal Added"
+            redirect_to user_goals_path(user_id: current_user.id)
+        else
+            flash[:error] = "Error Occured"
+            redirect_to new_user_goals_path(user_id: current_user.id)
+        end
+        
+    end
+    
+    
     def show
         
         @goals = Goal.all 
+        
     end
 
+    
+    # Whitelisting form fields for protection
+    private
+        def goal_params
+            params.require(:goal).permit(:goal_type, :goal_name, :start, :current, :end_goal, :start_date, :current_date, :end_date) 
+        end
     
 end
