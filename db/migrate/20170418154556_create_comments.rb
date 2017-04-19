7d0daca9f1313e78c62f413a70d6a08290b32f91
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-04-19 09:36:56 +0000

    Added comment model

---
+++ db/migrate/20170418154556_create_comments.rb
@@ -0,0 +1,9 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.integer :user_id
+      t.integer :goal_id
+      t.string :comment
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418084104) do
+ActiveRecord::Schema.define(version: 20170418154556) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "goal_id"
+    t.string  "comment"
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
